154b7a5fb7d2a2a3d9f969d1c6f67ab7dfbb5790
monkeywantbanana <caolanfleming@googlemail.com>
2019-10-06 11:55:28 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,66 @@
+import requests, json, ChannelFinder, re, threading, time
+from ChannelFinder import *
+from bs4 import BeautifulSoup
+
+def find_channels(keyword):
+    crawler = ChannelFinder()
+    return crawler.crawl(depth, keyword)
+
+f = open('second_kws.txt', 'r')
+second_kws = f.readlines()
+f.close()
+
+i = 0
+depth = 500
+keyword = input('Keyword : ')
+print('ok')
+videos = find_channels(keyword)
+
+emails = []
+
+def scrape(video_url):
+    r = requests.get(url=video_url)
+    channel_url = re.findall('http://www.youtube.com/channel/[A-Za-z0-9-_]+', r.content.decode('utf-8'))
+    if not channel_url:
+        channel_url = re.findall('http://www.youtube.com/user/[A-Za-z0-9-_]+', r.content.decode('utf-8'))
+    if channel_url:
+        channel_url = channel_url[0]
+        
+        r = requests.get(url=channel_url+'/about')
+        newemails = re.findall("([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", r.content.decode('utf-8'))
+        if newemails:
+            if [channel_url, newemails[0]] not in emails:
+                emails.append([channel_url, newemails[0]])
+
+print('here')
+
+i = 0
+threads = []
+while True:
+    threads = [t for t in threads if t.is_alive()]
+    
+    if len(threads) < 100 and i<len(videos):
+        URL = 'https://youtube.com'+videos[i]
+        if i%20 == 0:
+            print(i, len(videos))
+        thread = threading.Thread(target=scrape, args=(URL,))
+        threads.append(thread)
+        thread.start()
+
+        i += 1
+
+    # Break when all threads are finished
+    flag = False
+    if i == len(videos):
+        flag = True
+        for t in threads:
+            if t.is_alive():
+                flag = False
+                break
+    if flag:
+        break
+
+text_file = open(keyword+"fsd.txt", "w")
+for email in emails:
+    text_file.write(email[0]+';'+email[1]+'\n')
+text_file.close()
